61b0cc3c7016cc927a16f8a6c49d1b59837fbe5f
Martha Wangui Mwangi <marthawm7252@gmail.com>
2024-11-29 21:16:59 +0300

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers,name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  console.log(findMatching(drivers,'Bobby'))
+
+
+  function fuzzyMatch(drivers, letters)
+  {
+
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  console.log(fuzzyMatch(drivers,"sa"))
+
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
